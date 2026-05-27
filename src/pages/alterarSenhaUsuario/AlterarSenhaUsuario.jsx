5c52d78a525c42f0dda7f47adb42dc5ae221f721
Leonardo chaves <leonardo.pchaves@senacsp.edu.br>
2026-05-27 11:51:02 -0300

    atualizando pages

--- src/pages/alterarSenhaUsuario/AlterarSenhaUsuario.jsx
+++ src/pages/alterarSenhaUsuario/AlterarSenhaUsuario.jsx
@@ -1,58 +1,9 @@
-import "./AlterarSenhaUsuario.css";
+import React from 'react'
 
 const AlterarSenhaUsuario = () => {
-    return (
-        <div>
-
-       
-            <main>
-
-                <div className="card">
-
-                    <h1>Alterar senha</h1>
-
-                    <p className="descricao">
-                        Atualize suas credenciais de acesso com segurança.
-                    </p>
-
-
-                    <form>
-
-                        <label>Senha atual</label>
-                        <input type="password" />
-
-                        <label>Nova senha</label>
-                        <input type="password" />
-
-                        <label>Confirmar nova senha</label>
-                        <input type="password" />
-
-
-                        <div className="alerta">
-                            Certifique-se de que sua senha contenha letras,
-                            números e símbolos. Nunca compartilhamos seus
-                            dados de acesso e sua privacidade é nossa prioridade absoluta.
-                        </div>
-
-
-                        <button>
-                            SALVAR NOVA SENHA
-                        </button>
-
-                        <a href="#" className="cancelar">
-                            Cancelar
-                        </a>
-
-                    </form>
-
-                </div>
-
-            </main>
-
-      
-
-        </div>
-    )
+  return (
+    <div>AlterarSenhaUsuario</div>
+  )
 }
 
-export default AlterarSenhaUsuario;
+export default AlterarSenhaUsuario
