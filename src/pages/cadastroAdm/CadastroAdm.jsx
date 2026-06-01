5c36c63cb9be531fe023496bc349c041414c2f5d
WECTON SOARES CARVALHO <wecton.scarvalho@senacsp.edu.br>
2026-06-01 09:19:59 -0300

    atualizando

--- src/pages/cadastroAdm/CadastroAdm.jsx
+++ src/pages/cadastroAdm/CadastroAdm.jsx
@@ -1,7 +1,49 @@
+import "./CadastroAdm.css";
+
 const CadastroAdm = () => {
   return (
-    <div>
-        <h1>Cadastro Adm</h1>
+    <div class="card">
+
+        <h1 class="title">Cadastro</h1>
+
+        <p class="subtitle">
+            Cadastre-se para acessar o site.
+        </p>
+
+        <div class="form-group">
+            <label>Nome Completo</label>
+            <input type="text"></input>
+        </div>
+
+        <div class="form-group">
+            <label>E-mail</label>
+            <input type="email"></input>
+        </div>
+
+        <div class="form-group">
+            <label>Senha</label>
+            <input type="password"></input>
+        </div>
+
+        <div class="form-group">
+            <label>Confirmar senha</label>
+            <input type="password"></input>
+        </div>
+
+        <div class="info">
+            Seus dados são protegidos por protocolos de segurança.
+            Não compartilhamos informações pessoais com terceiros.
+        </div>
+
+        <button class="btn">
+            Cadastrar conta →
+        </button>
+
+        <div class="login">
+            Já possui uma conta?
+            <a href="#">Clique para fazer login</a>
+        </div>
+
     </div>
   )
 }
