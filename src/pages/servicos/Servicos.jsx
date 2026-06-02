b19214ecdceed5ff517d22ffacd3aca8d95d6cb0
GRAZIELE ALVES SOARES <graziele.asoares@senacsp.edu.br>
2026-06-02 08:53:05 -0300

    Serviços

--- src/pages/servicos/Servicos.jsx
+++ src/pages/servicos/Servicos.jsx
@@ -1,15 +1,16 @@
 import "./Servicos.css"
+import img from '../../assets/img.png';
+import img_novo from '../../assets/img_novo.png'
 
 const Servicos = () => {
   return (
     <>
-      <div class="container">
+      <div className="container">
         <h1>Serviços</h1>
-      </div>
 
       <div className="services-grid">
         <div className="bubble">
-          <img src="img/jpeg 1.png" alt="Suporte emocional" className="bubble-img" />
+          <img src={img}alt="Suporte emocional" className="bubble-img" />
           <p>Oferecer suporte emocional e orientação para ajudar no processo a lidar com desafios pessoais,
             profissionais e sociais. Auxiliar no autoconhecimento dos pensamentos, sentimentos e comportamentos,
             promovendo o autoconhecimento e bem-estar, sempre respeitando a ética e a individualidade de cada
@@ -17,11 +18,12 @@ const Servicos = () => {
         </div>
 
         <div className="bubble">
-          <img src="img/jpeg novo.png" alt="Processo terapêutico" className="bubble-img" />
+          <img src={img_novo} alt="Processo terapêutico" className="bubble-img" />
           <p>Não trabalho com respostas prontas ou soluções mágicas. O processo terapêutico é construído gradualmente
             e permite que você se compreenda de forma mais profunda, identificando causas internas das suas
             angústias e mudando a maneira como vive e se relaciona.</p>
         </div>
+      </div>
       </div>
     </>
   )
