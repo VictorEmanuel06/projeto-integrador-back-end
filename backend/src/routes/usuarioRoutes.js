b71d0f261f77fd63d1a71d44bb20ccabb2842c18
VICTOR EMANUEL DOS SANTOS ROCHA <victor.esrocha@senacsp.edu.br>
2026-07-29 09:58:57 -0300

    bloquando rota

--- backend/src/routes/usuarioRoutes.js
+++ backend/src/routes/usuarioRoutes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 import { usuarios } from "../controllers/usuariosController.js";
+import { verificarLogin } from "../middlewares/auth.js";
 
 const router = Router();
 
-router.get("/usuarioscadastrados", usuarios)
+router.get("/usuarioscadastrados", verificarLogin,usuarios)
 
 export default router;
